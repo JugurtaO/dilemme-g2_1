4a460057b3806e392c4d9a81b34fd04518f854bb
JugurtaO <jugurtaourzik@gmail.com>
2024-11-05 19:12:18 +0100

    +dev: resolved conflicts

--- src/main/java/fr/uga/l3miage/pc/prisonersdilemma/models/GameEncounter.java
+++ src/main/java/fr/uga/l3miage/pc/prisonersdilemma/models/GameEncounter.java
@@ -1,11 +1,6 @@
 package fr.uga.l3miage.pc.prisonersdilemma.models;
-
-import fr.uga.l3miage.pc.prisonersdilemma.models.states.GameEncounterStateInterface;
-import fr.uga.l3miage.pc.prisonersdilemma.models.states.WaitingState;
 import fr.uga.l3miage.pc.prisonersdilemma.utils.Utils;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -17,10 +12,7 @@ public  class GameEncounter {
     private int nbTours;
     private Player player1;
     private Player player2;
-    private GameEncounterStateInterface state;
-    private WebSocketSession player1Session;
-    private WebSocketSession player2Session;
-    private WebSocketSession currentSession;
+
 
     private List<Tour> history;
 
@@ -31,48 +23,10 @@ public  class GameEncounter {
         this.player1=p1;
         this.player2=p2;
         this.history=new ArrayList<>();
-        this.state = new WaitingState();
+
     }
 
-    public void setPlayer1Session(WebSocketSession session) {
-        this.player1Session = session;
-    }
 
-    public void setPlayer2Session(WebSocketSession session) {
-        this.player2Session = session;
-    }
-
-    public WebSocketSession getPlayer1Session() {
-        return player1Session;
-    }
-
-    public WebSocketSession getPlayer2Session() {
-        return player2Session;
-    }
-
-    public void setCurrentSession(WebSocketSession session) {
-        this.currentSession = session;
-    }
-
-    public WebSocketSession getCurrentSession() {
-        return currentSession;
-    }
-
-    public void setState(GameEncounterStateInterface state) {
-        this.state = state;
-    }
-
-    public void joinGame() throws IOException {
-        state.joinGame(this);
-    }
-
-    public void startGame() throws IOException {
-        state.startGame(this);
-    }
-
-    public void endGame() throws IOException {
-        state.endGame(this);
-    }
 
     public List<Tour> getHistory(){
         return history;
