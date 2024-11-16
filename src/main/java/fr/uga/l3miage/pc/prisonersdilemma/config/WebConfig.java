54002a6d3c3485f7081b9f8f53137c29bba8b08e
JugurtaO <jugurtaourzik@gmail.com>
2024-11-16 20:41:56 +0100

    +conteneurisation-dokcer: CI/CD overall done & deployment into AWS EC2 instance

---
+++ src/main/java/fr/uga/l3miage/pc/prisonersdilemma/config/WebConfig.java
@@ -0,0 +1,19 @@
+package fr.uga.l3miage.pc.prisonersdilemma.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")  // Replace with your specific frontend domain in production
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+    }
+}
