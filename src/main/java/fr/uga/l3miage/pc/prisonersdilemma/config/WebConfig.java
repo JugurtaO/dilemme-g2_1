300c81833ecf21b26910398d53df73d6676058e8
JugurtaO <jugurtaourzik@gmail.com>
2024-11-24 17:46:45 +0100

    final pipeline ready

--- src/main/java/fr/uga/l3miage/pc/prisonersdilemma/config/WebConfig.java
+++ src/main/java/fr/uga/l3miage/pc/prisonersdilemma/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")  // Replace with your specific frontend domain in production
+                .allowedOrigins("http://localhost:4000","http://localhost:4200")  // Replace with your specific frontend domain in production
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
