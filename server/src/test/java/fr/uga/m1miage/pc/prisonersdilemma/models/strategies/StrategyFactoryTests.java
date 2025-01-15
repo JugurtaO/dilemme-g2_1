4b136cec9b68c678d54eb7e845e7ca23dd7fa8b4
Miel Nils <nils.miel@etu.grenoble-alpes.fr>
2025-01-15 17:44:59 +0100

    test(Strat): fix coverage

--- server/src/test/java/fr/uga/m1miage/pc/prisonersdilemma/models/strategies/StrategyFactoryTests.java
+++ server/src/test/java/fr/uga/m1miage/pc/prisonersdilemma/models/strategies/StrategyFactoryTests.java
@@ -1,5 +1,6 @@
 package fr.uga.m1miage.pc.prisonersdilemma.models.strategies;
 
+import fr.uga.m1miage.pc.prisonersdilemma.models.strategies.external.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -46,4 +47,100 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
         boolean isRightInstance=StrategyFactory.getStrategyInstance(14) instanceof PavlovStrategyRandom;
         assertTrue(isRightInstance);
     }
+     @Test
+     void getInstance15() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(15) instanceof AdaptativStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance16() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(16) instanceof GradualStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance17() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(17) instanceof DonnantDonnantSuspiciousStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance18() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(18) instanceof SweetResentfulStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance19() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(19) instanceof ExternalDonnantDonnantStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance20() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(20) instanceof ExternalDonnantDonnantRandomStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance21() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(21) instanceof ExternalDonnantForTwoDonnantsRandomStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance22() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(22) instanceof ExternalDonnantForTwoDonnantsStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance23() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(23) instanceof ExternalNaiveSounderStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance24() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(24) instanceof ExternalRepentantSounderStrategy;
+         assertTrue(isRightInstance);
+     }
+
+     @Test
+     void getInstance25() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(25) instanceof ExternalNaivePeaceMakerStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance26() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(26) instanceof ExternalTruePeacemakerStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance27() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(27) instanceof ExternalRandomStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance28() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(28) instanceof ExternalAlwaysBetrayStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance29() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(29) instanceof ExternalAlwaysCooperateStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance30() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(30) instanceof ExternalResentfulStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance31() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(31) instanceof ExternalPavlovStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance32() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(32) instanceof ExternalPavlovRandomStrategy;
+         assertTrue(isRightInstance);
+     }
+     @Test
+     void getInstance33() {
+         boolean isRightInstance=StrategyFactory.getStrategyInstance(33) instanceof ExtenalAdaptativStrategy;
+         assertTrue(isRightInstance);
+     }
 }
